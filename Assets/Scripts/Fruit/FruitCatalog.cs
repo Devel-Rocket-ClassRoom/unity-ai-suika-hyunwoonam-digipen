8d8fdcd0ee3e68e216e039e7b5723429c3359861
DESKTOP-NJGSL36\ok623 <hyunwoo.nam@digipen.edu>
2026-05-29 18:07:11 +0900

    미션 3

--- Assets/Scripts/Fruit/FruitCatalog.cs
+++ Assets/Scripts/Fruit/FruitCatalog.cs
@@ -32,7 +32,7 @@ namespace SuikaGame.Fruit
                 new FruitDefinition(
                     1,
                     "Cherry",
-                    0.23f,
+                    0.46f,
                     1,
                     new Color(0.83f, 0.12f, 0.18f),
                     SuikaAssetProvider.LoadFruitSprite(1)
@@ -40,7 +40,7 @@ namespace SuikaGame.Fruit
                 new FruitDefinition(
                     2,
                     "Strawberry",
-                    0.29f,
+                    0.58f,
                     3,
                     new Color(0.94f, 0.18f, 0.22f),
                     SuikaAssetProvider.LoadFruitSprite(2)
@@ -48,7 +48,7 @@ namespace SuikaGame.Fruit
                 new FruitDefinition(
                     3,
                     "Grape",
-                    0.36f,
+                    0.72f,
                     6,
                     new Color(0.52f, 0.28f, 0.74f),
                     SuikaAssetProvider.LoadFruitSprite(3)
@@ -56,7 +56,7 @@ namespace SuikaGame.Fruit
                 new FruitDefinition(
                     4,
                     "Dekopon",
-                    0.45f,
+                    0.9f,
                     10,
                     new Color(0.96f, 0.49f, 0.17f),
                     SuikaAssetProvider.LoadFruitSprite(4)
@@ -64,7 +64,7 @@ namespace SuikaGame.Fruit
                 new FruitDefinition(
                     5,
                     "Persimmon",
-                    0.55f,
+                    1.1f,
                     15,
                     new Color(0.91f, 0.38f, 0.13f),
                     SuikaAssetProvider.LoadFruitSprite(5)
@@ -72,7 +72,7 @@ namespace SuikaGame.Fruit
                 new FruitDefinition(
                     6,
                     "Apple",
-                    0.68f,
+                    1.36f,
                     21,
                     new Color(0.78f, 0.12f, 0.15f),
                     SuikaAssetProvider.LoadFruitSprite(6)
@@ -80,7 +80,7 @@ namespace SuikaGame.Fruit
                 new FruitDefinition(
                     7,
                     "Pear",
-                    0.83f,
+                    1.66f,
                     28,
                     new Color(0.78f, 0.82f, 0.35f),
                     SuikaAssetProvider.LoadFruitSprite(7)
@@ -88,7 +88,7 @@ namespace SuikaGame.Fruit
                 new FruitDefinition(
                     8,
                     "Peach",
-                    1.00f,
+                    2.00f,
                     36,
                     new Color(0.96f, 0.49f, 0.56f),
                     SuikaAssetProvider.LoadFruitSprite(8)
@@ -96,7 +96,7 @@ namespace SuikaGame.Fruit
                 new FruitDefinition(
                     9,
                     "Pineapple",
-                    1.20f,
+                    2.40f,
                     45,
                     new Color(0.95f, 0.74f, 0.19f),
                     SuikaAssetProvider.LoadFruitSprite(9)
@@ -104,7 +104,7 @@ namespace SuikaGame.Fruit
                 new FruitDefinition(
                     10,
                     "Melon",
-                    1.43f,
+                    2.86f,
                     55,
                     new Color(0.58f, 0.86f, 0.43f),
                     SuikaAssetProvider.LoadFruitSprite(10)
@@ -112,7 +112,7 @@ namespace SuikaGame.Fruit
                 new FruitDefinition(
                     11,
                     "Watermelon",
-                    1.70f,
+                    3.40f,
                     66,
                     new Color(0.13f, 0.58f, 0.27f),
                     SuikaAssetProvider.LoadFruitSprite(11)
